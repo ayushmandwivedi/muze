fb633267c04bacf7017ac86d337df75c9a51be08
ayushmandwivedi <ayushman14dwivedi@gmail.com>
2025-05-29 17:16:22 +0530

    adds route handler for creating stream

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,6 +1,7 @@
 import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
+const ytRegex = new RegExp("https://www.youtube.com/watch?v=[w-]+&t=d+s");
 
 const createStringSchema = z.object({
   creatorId: z.string(),
@@ -8,12 +9,8 @@ const createStringSchema = z.object({
 });
 
 export async function POST(req: NextRequest) {
-  try {
-    const data = createStringSchema.safeParse(await req.json());
-    prismaClient.stream.create({
-      user,
-    });
-  } catch (e) {
+  const result = createStringSchema.safeParse(await req.json());
+  if (!result.success) {
     return NextResponse.json(
       {
         msg: "Error while adding a stream",
@@ -23,4 +20,23 @@ export async function POST(req: NextRequest) {
       }
     );
   }
+  const isYT = ytRegex.test(result.data.url);
+  if (!isYT) {
+    return NextResponse.json(
+      {
+        message: "Error",
+      },
+      {
+        status: 411,
+      }
+    );
+  }
+
+  const extractedId = result.data.url.split("?v=")[1];
+  await prismaClient.stream.create({
+    user: result.data.creatorId,
+    url: result.data.url,
+    extractedId: extractedId,
+    type: "Youtube",
+  });
 }
